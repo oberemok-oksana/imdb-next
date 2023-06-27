53cf64630fca457687440986dac2b0dbbdd7c726
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-27 20:27:50 +0700

    add config for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/w500/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+//`https://image.tmdb.org/t/p/w500${movie.poster_path}`;
