ee46896a8a4a468c3e3366fabb54d14bcfb68c8b
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-27 15:34:59 +0700

    add static ol with static cards

--- app/top-20/page.tsx
+++ app/top-20/page.tsx
@@ -1,9 +1,21 @@
+import MovieCard from "../components/movieCard/MovieCard";
 import styles from "./TopMovies.module.css";
 
 const TopMovies = () => {
   return (
-    <div>
+    <div className={styles.main}>
       <h1 className={styles.title}>Top 20</h1>
+      <ol className={styles.list}>
+        <li>
+          <MovieCard />
+        </li>
+        <li>
+          <MovieCard />
+        </li>
+        <li>
+          <MovieCard />
+        </li>
+      </ol>
     </div>
   );
 };
