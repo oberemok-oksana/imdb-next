a3d590a1bf94fa2f039e7f076586a16f36b1461b
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-27 14:17:06 +0700

    make new page Top-20

---
+++ app/top-20/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./TopMovies.module.css";
+
+const TopMovies = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Top 20</h1>
+    </div>
+  );
+};
+
+export default TopMovies;
