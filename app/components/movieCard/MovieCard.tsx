945e1ef5eb677630867a638db69e0837d8a75efd
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-27 20:31:14 +0700

    create MovieCardProps type, use it, implement rendering movie

--- app/components/movieCard/MovieCard.tsx
+++ app/components/movieCard/MovieCard.tsx
@@ -1,22 +1,35 @@
 import Image from "next/image";
 import styles from "./MovieCard.module.css";
+import { MovieType } from "@/app/types";
 
-const MovieCard = () => {
+type MovieCardProps = {
+  movie: MovieType;
+};
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+  const src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
   return (
     <div className={styles.card}>
-      <Image
-        src="/images/tIgSd8DhLqHlUaeEjVXc3189zAQ.jpg"
-        alt="John Wheek"
-        width="40"
-        height="55"
-      />
-      <div>
-        John Wheek 4 <span>(2023)</span>
+      <div className={styles.parts}>
+        <Image src={src} alt={movie.name} width="80" height="90" />
+        <div>
+          {movie.name} <span>({movie.first_air_date.slice(0, 4)})</span>
+        </div>
       </div>
-      <div>
-        OMDb Rating <span>9.2</span>
+      <div className={styles.align}>
+        <div className={styles.align}>
+          OMDb Rating
+          <Image
+            src="/images/icons8-star-48.png"
+            alt="rating star"
+            width="30"
+            height="30"
+          />
+          <span> {movie.vote_average}</span>
+        </div>
+        <button className={styles.button}>+</button>
       </div>
-      <button>+</button>
     </div>
   );
 };
