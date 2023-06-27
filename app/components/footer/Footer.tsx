ddff459d584e6a82d07078c085ed88151f088361
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-27 15:23:16 +0700

    implement footer with style

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.socials}>
+        <div className={styles.social}>
+          <a target="_blank" href="https://www.facebook.com/">
+            <Image src="/images/facebook.svg" alt="" width="20" height="20" />
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a target="_blank" href="https://www.instagram.com/">
+            <Image src="/images/instagram.svg" alt="" width="20" height="20" />
+          </a>
+        </div>
+        <div className={styles.social}>
+          <a target="_blank" href="https://twitter.com/">
+            <Image src="/images/twitter.svg" alt="" width="20" height="20" />
+          </a>
+        </div>
+      </div>
+      <div>2023</div>
+    </footer>
+  );
+};
+
+export default Footer;
