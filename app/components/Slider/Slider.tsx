b315a3b00f952e703cd6b0e91260c99a67730643
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-28 17:28:09 +0700

    change arrow buttons

--- app/components/Slider/Slider.tsx
+++ app/components/Slider/Slider.tsx
@@ -8,10 +8,42 @@ type SliderPropsType = {
   data: MovieType[];
 };
 
+const buttonStyle = {
+  width: "30px",
+  background: "none",
+  border: "0px",
+  top: "34%",
+};
+
+const properties = {
+  prevArrow: (
+    <button style={{ ...buttonStyle }}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        fill="rgb(249 226 236 / 80%)"
+      >
+        <path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
+      </svg>
+    </button>
+  ),
+  nextArrow: (
+    <button style={{ ...buttonStyle }}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        fill="rgb(249 226 236 / 80%)"
+      >
+        <path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
+      </svg>
+    </button>
+  ),
+};
+
 const Slideshow = ({ data }: SliderPropsType) => {
   return (
     <div className={styles["slide-container"]}>
-      <Fade>
+      <Fade {...properties}>
         {data.map((movie, index) => (
           <div key={index}>
             <img
