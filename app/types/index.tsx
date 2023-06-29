eeebf9ef224f55ab762a1182c92b5a8301a601c0
Oksana Oberemok <Pylypenkok@gmail.com>
2023-06-29 20:11:42 +0700

    add type for find movie by id

--- app/types/index.tsx
+++ app/types/index.tsx
@@ -13,3 +13,59 @@ export type MovieType = {
   vote_average: number;
   vote_count: number;
 };
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+export type ProductionCompany = {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+};
+
+export type ProductionCountry = {
+  iso_3166_1: string;
+  name: string;
+};
+
+export type SpokenLanguage = {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+};
+
+export type FoundByIdType = {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    id: number | string;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number | string;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
