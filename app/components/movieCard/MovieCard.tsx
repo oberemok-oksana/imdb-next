4f69532fd95ad2dd7ae8f1047f14db87a5720509
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-03 19:22:50 +0700

    fix error with movie.name vs movie.title, fix src

--- app/components/movieCard/MovieCard.tsx
+++ app/components/movieCard/MovieCard.tsx
@@ -1,36 +1,54 @@
 import Image from "next/image";
 import styles from "./MovieCard.module.css";
 import { MovieType } from "@/app/types";
+import Link from "next/link";
 
 type MovieCardProps = {
   movie: MovieType;
 };
 
 const MovieCard = ({ movie }: MovieCardProps) => {
-  const src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const src = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "/images/poster_default.jpg";
 
   return (
-    <div className={styles.card}>
-      <div className={styles.parts}>
-        <Image src={src} alt={movie.name} width="80" height="90" />
-        <div>
-          {movie.name} <span>({movie.first_air_date.slice(0, 4)})</span>
-        </div>
-      </div>
-      <div className={styles.align}>
-        <div className={styles.align}>
-          OMDb Rating
+    <Link href={`/movies/${movie.id}`}>
+      <div className={styles.card}>
+        <div className={styles.parts}>
           <Image
-            src="/images/icons8-star-48.png"
-            alt="rating star"
-            width="30"
-            height="30"
+            src={src}
+            alt={movie.name || movie.title}
+            width="80"
+            height="90"
           />
-          <span> {movie.vote_average}</span>
+          <div>
+            {movie.name || movie.title}{" "}
+            {
+              <span>
+                (
+                {movie.first_air_date?.slice(0, 4) ||
+                  movie.release_date?.slice(0, 4)}
+                )
+              </span>
+            }
+          </div>
         </div>
-        <button className={styles.button}>+</button>
+        <div className={styles.align}>
+          <div className={styles.align}>
+            OMDb Rating
+            <Image
+              src="/images/icons8-star-48.png"
+              alt="rating star"
+              width="30"
+              height="30"
+            />
+            <span> {movie.vote_average?.toFixed(1)}</span>
+          </div>
+          <button className={styles.button}>+</button>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
