5960f88c27849c3bdb776704c3794b47f4d36266
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-03 17:18:47 +0700

    implement search

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,13 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./Header.module.css";
+import { redirect } from "next/navigation";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const key =
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZWZlNzUyN2ZmM2EzZmFjOGE2MTAzMzAxMzU3MjE2OSIsInN1YiI6IjYwOWU2OGI0ODA3Mjk4MDAyOWE1MGI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xbowz5LfSHTk6wVl3Mk8ixotglAi_tNU9rAzz0qk3gk";
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${search}`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => result.results)
+      .catch((error) => console.log(error));
+  }, [search]);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  // const submitSearch = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log("I saved the data");
+  //   redirect("/search");
+  // };
+
   return (
     <header className={styles.header}>
       <div>
         <a href="/">OMDB</a>
       </div>
-      <form className={styles.form}>
-        <input type="text" placeholder="Search" className={styles.input} />
+      <form className={styles.form} action="/search">
+        <input
+          name="query"
+          type="text"
+          placeholder="Search"
+          className={styles.input}
+          onChange={handleInput}
+        />
         <button type="submit" className={styles.button}>
           Search
         </button>
