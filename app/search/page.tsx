34d6011e8c200c736ff5c0c89c785e84daafa375
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-03 19:21:46 +0700

    add fetch to find a movie by name, add query to search input

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,17 +1,45 @@
-type FoundMoviesPropsType = {
-  searchParams: {
-    query: string | number | undefined;
-  };
+import Link from "next/link";
+import MovieCard from "../components/movieCard/MovieCard";
+import { FoundMoviesPropsType, MovieType } from "../types";
+
+const findMoviesByName = async (search: string | number | undefined) => {
+  const key =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZWZlNzUyN2ZmM2EzZmFjOGE2MTAzMzAxMzU3MjE2OSIsInN1YiI6IjYwOWU2OGI0ODA3Mjk4MDAyOWE1MGI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xbowz5LfSHTk6wVl3Mk8ixotglAi_tNU9rAzz0qk3gk";
+  const result = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${search}`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+    }
+  );
+
+  if (!result.ok) {
+    throw new Error(result.statusText);
+  }
+
+  const data = await result.json();
+  return data;
 };
 
-const FoundMovies = ({ searchParams }: FoundMoviesPropsType) => {
+const FoundMovies = async ({ searchParams }: FoundMoviesPropsType) => {
   const search = searchParams?.query;
-  console.log(
-    "=======================================" +
-      search +
-      "========================================"
+  const movies = await findMoviesByName(search);
+
+  return (
+    <div>
+      <h1>Here what I've found:</h1>
+      <ol>
+        {movies?.results?.map((movie: MovieType) => (
+          <li key={movie.id}>
+            <Link href="/"></Link>
+            <MovieCard movie={movie} />
+          </li>
+        ))}
+      </ol>
+    </div>
   );
-  return <div>I found some of them..</div>;
 };
 
 export default FoundMovies;
