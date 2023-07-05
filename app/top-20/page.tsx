a630bfe361905eb894d3f0e7223eda5b876eea5f
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-05 20:25:54 +0700

    move getTopMovies to api folder

--- app/top-20/page.tsx
+++ app/top-20/page.tsx
@@ -1,25 +1,7 @@
+import { getTopMovies } from "../api";
 import MovieCard from "../components/movieCard/MovieCard";
-import { MovieType } from "../types";
+
 import styles from "./TopMovies.module.css";
-
-const getTopMovies = async (): Promise<MovieType[]> => {
-  const key =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZWZlNzUyN2ZmM2EzZmFjOGE2MTAzMzAxMzU3MjE2OSIsInN1YiI6IjYwOWU2OGI0ODA3Mjk4MDAyOWE1MGI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xbowz5LfSHTk6wVl3Mk8ixotglAi_tNU9rAzz0qk3gk";
-  const result = await fetch("https://api.themoviedb.org/3/movie/popular", {
-    headers: {
-      Authorization: `Bearer ${key}`,
-      Accept: "application/json",
-    },
-  });
-
-  if (!result.ok) {
-    throw new Error(result.statusText);
-  }
-
-  const data = await result.json();
-
-  return data.results;
-};
 
 const TopMovies = async () => {
   const data = await getTopMovies();
