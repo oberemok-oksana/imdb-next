d2c4aafcd304b600886345ea056bc0c1d1ae238d
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-12 13:35:39 +0700

    delete getToken func

--- app/api/index.tsx
+++ app/api/index.tsx
@@ -3,18 +3,6 @@ import { MovieType } from "../types";
 const key =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZWZlNzUyN2ZmM2EzZmFjOGE2MTAzMzAxMzU3MjE2OSIsInN1YiI6IjYwOWU2OGI0ODA3Mjk4MDAyOWE1MGI5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xbowz5LfSHTk6wVl3Mk8ixotglAi_tNU9rAzz0qk3gk";
 
-export const getToken = (): Promise<string> => {
-  return fetch("https://api.themoviedb.org/3/authentication/token/new", {
-    headers: {
-      Accept: "application/json",
-      Authorization: `Bearer ${key}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => data.request_token)
-    .catch((err) => console.error(err));
-};
-
 export const getTopMovies = async (): Promise<MovieType[]> => {
   const result = await fetch("https://api.themoviedb.org/3/movie/popular", {
     headers: {
